2e4e50904d8dc1f7511529d06d33220043affa2f
Noshaid Ali <noshaid.ali@cricingif.com>
2020-03-20 23:08:51 +0500

    Initial Commit

---
+++ SwiftUI Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Calculator
+//
+//  Created by Noshaid Ali on 20/03/2020.
+//  Copyright © 2020 Noshaid Ali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
